fe845134830846422effb33a00654293f70bf3ef
Harry-LamNgo <harrylam.ngo115@gmail.com>
2026-05-18 17:42:15 +0700

    Update main.py -- AI agent can response with user input, and create list of messages for AI agent to look back the conversation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -11,9 +13,18 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    # Using argparse to create an object that receive any user text (str type)
+    # we telling the argument parser to expect a single positional argument
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")        # Name the argument as user_prompt - type string
+    args = parser.parse_args()
+
+    # Using types from google.genai - to create a list of user_prompt
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages,
     )
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
