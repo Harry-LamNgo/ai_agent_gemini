bf83a79da1d0fa390f348279ab4454519bdce9b9
Harry-LamNgo <harrylam.ngo115@gmail.com>
2026-05-19 17:13:36 +0700

    Update main.py -- Add verbose mode - prompt token and response token is in verbose mode, add guard in case usage_metadata is empty or None type

--- main.py
+++ main.py
@@ -17,17 +17,30 @@ def main():
     # we telling the argument parser to expect a single positional argument
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")        # Name the argument as user_prompt - type string
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-
-    # Using types from google.genai - to create a list of user_prompt
+    
+    # Using types from google.genai - to create a list of user_prompt -- this list is expand in the future (long conversation)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+    content_generated(client, messages, args.verbose)
+
+def content_generated(client, messages, verbose):
 
     response = client.models.generate_content(
         model='gemini-2.5-flash',
         contents=messages,
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    # This is to guard whenever the Object-usage_metadata from Gemini is empty or None type
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+    
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response: ")
     print(response.text)
 
