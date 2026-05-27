5bfff458a2ff7b1a75849d63a89f787e70358bd7
Harry-LamNgo <harrylam.ngo115@gmail.com>
2026-05-27 17:29:00 +0700

    Update run_python_file.py -- Update if condition more clearly and add comment about the rule for subprocess.run - explaination of subprocess properties

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -17,8 +17,21 @@ def run_python_file(
             return f'Error: "{file_path}" is not a Python file'
         
         command = ["python", abs_file_path]
-        if args != None and len(args) != 0:
+        if args is not None and len(args) != 0:     # can use "if args:" due to Python treats both None and [] as falsy -- this is better because it's not care about order (always put the null check first)
             command.extend(args)
+        
+        '''
+        ## rule for run_python_file:
+        1. only allow the LLM to run code in a specific dir (working_directory == abs_work_dir)
+            ---> cwd = abs_work_dir
+        2. use a 30-second timeout to prevent it from running indefinitely
+            ---> timeout = 30
+
+        subprocess.run properties:
+        args = command (the input positional arguments), cwd=abs_work_dir (set the dir the subprocess runs in)
+        capture_output = True (tell python to capture both stdout and stderr from subprocess to use them)
+        text = True (by default, subprocess return raw bytes - this setting decodes the output to strings using the default encoding -- usually UTF-8)
+        '''
 
         completed_process = subprocess.run(command, cwd=abs_work_dir, capture_output=True , text=True, timeout=30)
 
@@ -27,11 +40,10 @@ def run_python_file(
             output_string.append(f'Process exited with code {completed_process.returncode}')
         if completed_process.stdout == "" and completed_process.stderr == "":
             output_string.append(f'No output produced')
-        else:
-            if completed_process.stdout != "":
-                output_string.append(f'STDOUT: {completed_process.stdout}')
-            if completed_process.stderr != "":
-                output_string.append(f'STDERR: {completed_process.stderr}')
+        if completed_process.stdout:
+            output_string.append(f'STDOUT: {completed_process.stdout}')
+        if completed_process.stderr:
+            output_string.append(f'STDERR: {completed_process.stderr}')
         
         return "\n".join(output_string)
     except Exception as e:
