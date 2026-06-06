47295e8420009787be1f57f4dfe10b30ba6bd2c9
Harry-LamNgo <harrylam.ngo115@gmail.com>
2026-06-06 13:59:52 +0700

    Update main.py, call_function.py and config.py -- Update main.py to make AI can run the function and send the result to user, update call_function.py create function name call_function for AI uses this to run the available functions its need for result, update WORK_DIR for config.py

--- call_function.py
+++ call_function.py
@@ -1,9 +1,12 @@
 from google.genai import types
+from collections.abc import Callable
 
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.write_file import schema_write_file
-from functions.run_python_file import schema_run_python_file
+from config import WORK_DIR
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.write_file import schema_write_file, write_file
+from functions.run_python_file import schema_run_python_file, run_python_file
+
 
 # This is where all schemas of function store
 # --> To organize the import function for main.py - having them gathered in one place is cleaner than scattering imports across files
@@ -15,3 +18,49 @@ available_functions = types.Tool(
     function_declarations=[schema_get_files_info, schema_get_file_content, 
                            schema_write_file, schema_run_python_file]
 )
+
+# Create a dictionary of function (function_map) - this dict can help you to fast get the value (in this case is function) according to the key
+# -> you can expand more functions in the future - without dict, you have to write at least 4 (more if there are more functions) if ... elif ... with the same pattern
+# --> dict can give you the way to look up fast ------>> function_map[function_name](**args)
+function_map: dict[str, Callable[..., str]] = {"get_file_content": get_file_content, "get_files_info": get_files_info
+                                                , "write_file": write_file, "run_python_file": run_python_file}
+
+
+def call_function(
+        function_call: types.FunctionCall, verbose: bool = False
+) -> types.Content:
+    if verbose:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
+
+    function_name = function_call.name or ""
+
+    if function_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[types.Part.from_function_response(
+                name=function_name,
+                response={"error": f"Unknown function: {function_name}"},
+            )],
+        )
+    
+    # Create a shallow copy of args of function_call and named it args if function_call.args is NONE then args is empty dict {}
+    args = dict(function_call.args) if function_call.args else {}
+
+    ## Fixed Working Directory to ./calculator --> Whenever need change need to go to call_function.py to change <---- Misuse feature of call_function.py (Not recommend)
+    # args["working_directory"] = "./calculator"
+
+    # Better approach --> Create a global variable where users can config that affect whole system  --> Config.py is where to users manual change their configs
+    # -> Import that variable from config.py
+    args["working_directory"] = WORK_DIR
+
+    function_result :str = function_map[function_name](**args)
+
+    return types.Content(
+        role="tool",
+        parts=[types.Part.from_function_response(
+            name=function_name,
+            response={"result": function_result}
+        )],
+    )
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 
 def main():
@@ -17,9 +18,9 @@ def main():
 
     # Using argparse to create an object that receive any user text (str type)
     # we telling the argument parser to expect a single positional argument
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")        # Name the argument as user_prompt - type string
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to AI model")        # Name the argument as user_prompt - type string
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose mode")
     args = parser.parse_args()
     
     # Using types from google.genai - to create a list of user_prompt -- this list is expand in the future (long conversation)
@@ -28,6 +29,8 @@ def main():
     if args.verbose:
         print(f"User prompt: {args.user_prompt}\n")
     content_generated(client, messages, args.verbose)
+
+
 
 def content_generated(client, messages, verbose):
 
@@ -46,11 +49,26 @@ def content_generated(client, messages, verbose):
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response: ")
+        
+    function_response: list[types.Part] = []
     if response.function_calls:
         for function_call in response.function_calls:
-            print(f'Calling function: {function_call.name}({function_call.args})')
+            # call_function - AI run the function need to run
+            result = call_function(function_call, verbose)
+
+            # If any information in .parts of the result object is empty or None -> Raise Error because no information to response
+            if (not result.parts 
+                or not result.parts[0].function_response 
+                or not result.parts[0].function_response.response):
+                raise RuntimeError(f"Empty function response for {function_call.name}")
+
+            if verbose:
+                print(f"-> {result.parts[0].function_response.response}")
+
+            function_response.append(result.parts[0])
+        
     else:
+        print("Response: ")
         print(response.text)
 
 
